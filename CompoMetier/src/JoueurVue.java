eacb02e18cd57a3c48b7a46922eaab3089554859
Baptiste <74326771+Chapdan@users.noreply.github.com>
2024-05-24 10:20:27 +0200

    Delete CompoMetier directory

--- CompoMetier/src/JoueurVue.java
+++
@@ -1,6 +0,0 @@
-public class JoueurVue {
-    public void afficherFicheJoueur(String prenom, int cash) {
-        System.out.println("Prénom: " + prenom);
-        System.out.println("Cash: " + cash + "€");
-    }
-}
